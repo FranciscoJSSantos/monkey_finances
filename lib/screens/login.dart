60b0a5596a556a4a4bb3dca872f65d328c8caaf3
FranciscoJSSantos <francisco.jsantana3@gmail.com>
2022-09-25 11:19:03 -0300

    pequenas alteracoes

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -264,18 +264,3 @@ class _LoginScreenState extends State<LoginScreen> {
     ));
   }
 }
-
-
-
-// decoration: const BoxDecoration(
-//                   gradient: LinearGradient(
-//                 begin: Alignment.topCenter,
-//                 end: Alignment.bottomCenter,
-//                 colors: [
-//                   Color(0xFF73AEF5),
-//                   Color(0xFF61A4F1),
-//                   Color(0xFF478DE0),
-//                   Color(0xFF398AE5)
-//                 ],
-//                 stops: [0.1, 0.4, 0.7, 0.9],
-//               ))
